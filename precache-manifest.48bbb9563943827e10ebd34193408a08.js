1001ead91ba3b78d14f8ab645976f2980de68560
lrojas <lujan.rojas@sondeos.com.ar>
2020-07-07 21:04:08 -0300

    Updates

---
+++ precache-manifest.48bbb9563943827e10ebd34193408a08.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71ee64048130b6fa8fa40d4903bc4ea",
+    "url": "/lushan-web/index.html"
+  },
+  {
+    "revision": "6cd1cf531e8477c2b97f",
+    "url": "/lushan-web/static/css/main.9407700d.chunk.css"
+  },
+  {
+    "revision": "1548e5a03acb881ba9ec",
+    "url": "/lushan-web/static/js/2.4215570b.chunk.js"
+  },
+  {
+    "revision": "6cd1cf531e8477c2b97f",
+    "url": "/lushan-web/static/js/main.4d87fb20.chunk.js"
+  },
+  {
+    "revision": "259d457785292b2555b2",
+    "url": "/lushan-web/static/js/runtime-main.6587f5d5.js"
+  },
+  {
+    "revision": "0d09daaced1c06184ce0a653886e8f1f",
+    "url": "/lushan-web/static/media/css.0d09daac.png"
+  },
+  {
+    "revision": "e37b9a47fe5eb890c84cc50ca8aece29",
+    "url": "/lushan-web/static/media/docker.e37b9a47.png"
+  },
+  {
+    "revision": "94c0bbd35361706d5cb94f0497a8bd61",
+    "url": "/lushan-web/static/media/gatito.94c0bbd3.png"
+  },
+  {
+    "revision": "acf172f26b6761ec94a0d4316bdbf6d4",
+    "url": "/lushan-web/static/media/github.acf172f2.png"
+  },
+  {
+    "revision": "bc25f5143ef5ba9db06e66a5fcb077fb",
+    "url": "/lushan-web/static/media/graphql.bc25f514.png"
+  },
+  {
+    "revision": "5d580e47c4c35318275563e545dcaf5c",
+    "url": "/lushan-web/static/media/html.5d580e47.png"
+  },
+  {
+    "revision": "0a364ba72bf16d5d0eae45d4645dbe23",
+    "url": "/lushan-web/static/media/mariobros.0a364ba7.png"
+  },
+  {
+    "revision": "94dd94253365d99fba1eeb43dd527072",
+    "url": "/lushan-web/static/media/mysql.94dd9425.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/lushan-web/static/media/nodejs.55e83371.png"
+  },
+  {
+    "revision": "cb7f8c0d37a10ff5ab4202986f9f0c64",
+    "url": "/lushan-web/static/media/reactjs.cb7f8c0d.png"
+  },
+  {
+    "revision": "fa62cbcf6d2388fe02e887919bdfa4cd",
+    "url": "/lushan-web/static/media/vuejs.fa62cbcf.png"
+  },
+  {
+    "revision": "100ee177ee2f0978bd583bc3753a0b5c",
+    "url": "/lushan-web/static/media/vuetify.100ee177.png"
+  }
+]);
